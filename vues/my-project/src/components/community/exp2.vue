a6630560c0ef38aba65f23c20c09c1bf38e61a67
archresolt <2442541843@qq.com>
2020-02-25 16:16:04 +0800

    2.25document

--- vues/my-project/src/components/community/exp2.vue
+++ vues/my-project/src/components/community/exp2.vue
@@ -1,8 +1,52 @@
 <template>
-    <div style="font-size: 30px; color:red;">这里是社区</div>
+    <div>
+        <phos></phos>
+        <div class="exp2part0">
+        <div class="exp2part1"></div>
+        <div class="exp2part2">
+            <photext></photext>
+        </div>
+        <div class="exp2part3"></div>
+        </div>
+    </div>
 </template>
+<style scoped>
+.exp2part0{
+    width:1440px;
+    margin:0 auto;
+}
+.exp2part1{
+    height: 95px;
+    width:840px;
+    margin-left: 120px;
+    float:left;
+    background:green;
+}
+.exp2part2{
+    width:840px;
+    height: 1170px;
+    float: left;
+    margin-left: 120px;
+    /* background: red; */
+}
+.exp2part3{
+    width:288px;
+height:754px;
+background:rgba(248,249,250,1);
+float: left;
+margin-left: 42px;
+margin-top:-63px;
+}
+</style>
 <script>
+import phos from "../communitycomponents/phos";
+import photext from "../communitycomponents/photext";
 export default {
-    
+    components:{
+phos:phos,
+photext:photext,
+    },
+
+  
 }
 </script>
---
+++ vues/my-project/src/components/communitycomponents/phos.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="part0">
+    <div class="part1">
+        <div class="part2">
+            <img src="@/assets/image/社区首页1/part1.png"/>
+            <div class="part3 font3">接收来自马尔代夫的快递</div>
+            <!-- @/assets/image/检索切图/Shape.png -->
+        </div>
+         <div class="part2 ">
+             <img src="@/assets/image/社区首页1/part2.png"/>
+              <div class="part3 font3"> 还有很多你不曾看过的世界 | 踮起脚尖在这里跳舞，怎样都好</div>
+         </div>
+          <div class="part2">
+              <img src="@/assets/image/社区首页1/part3.png"/>
+               <div class="part3 font3">在世界尽头看日落</div>
+          </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+/* @import "../css/fontstyle.css"; */
+@import "../../assets/css/fontstyle1.css";
+.part0 {
+  width: 1440px;
+  height: 348px;
+  margin: 0 auto;
+  background: #f8f9fa;
+  position: relative;
+}
+.part1 {
+  width: 1200px;
+  height: 228px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+}
+.part2 {
+  width: 360px;
+  height: 228px;
+  position: relative;
+  float: left;
+  margin-left: 30px;
+  /* background: red; */
+}
+.part3{
+    position: absolute;
+    margin-top:-57px;
+    margin-left: 10px;
+   width:330px;
+height:44px;
+}
+</style>
